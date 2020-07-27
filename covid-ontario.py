1ea93345d0cd98e871d1fb4b616a4aa57821bbbe
Kendra Oudyk <kendra.oudyk@gmail.com>
2020-07-27 16:44:26 -0400

    remove code with default color scheme

--- covid-ontario.py
+++ covid-ontario.py
@@ -135,186 +135,7 @@ datetimes = datetimes[:-days_in_range]
 data.head(4) # visualize the first few rows
 # -
 
-# ## Visualize the timeline for each PHU in a separate plot
-
-# +
-# visualize
-matplotlib.rc('font', size = 22)
-matplotlib.rc('figure', max_open_warning = 0)
-
-ymax = np.max(data.loc[:, data.columns != 'Date'].values) + 2
-ymin = - .03
-linewidth = 4
-
-alpha = .1
-days = 14
-for phu in phus:
-    
-    p = data[phu]
-    t = datetimes
-    
-    fig, ax = plt.subplots(figsize=(20, 10))
-    
-    ax.plot(datetimes, p, color='black', linewidth=linewidth)
-    
-    ax.axhspan(ymin, 0.05*days, facecolor='blue', alpha=alpha)
-    ax.axhspan(0.05*days, 0.1*days, facecolor='green', alpha=alpha)
-    ax.axhspan(0.1*days, 0.25*days, facecolor='yellow', alpha=alpha)
-    ax.axhspan(0.25*days, 0.5*days, facecolor='darkorange', alpha=alpha)
-    ax.axhspan(0.5*days, ymax, facecolor='red', alpha=alpha)
-    
-    plt.ylim([ymin, ymax])
-    plt.xlabel('Date')
-    plt.ylabel('# infected per 10,000\nin past 14 days')
-    plt.title('%s' %phu)
-# -
 # ## Visualize the timelines for all PHUs on the same interactive plot
-# *Color/line scheme A*
-
-# +
-# This code was based on https://stackoverflow.com/questions/31410043/hiding-lines-after-showing-a-pyplot-figure/31417070
-
-# %matplotlib notebook
-
-matplotlib.rc('font', size = 14)
-
-linewidth=4
-alpha=.1
-ymax = np.max(data.loc[:, data.columns != 'Date'].values) + 2
-ymin = - .2
-days = 14
-
-def main():
-    
-    NUM_COLORS = 34
-    LINE_STYLES = ['solid', 'dashed', 'dashdot', 'dotted']
-    COLORS = ['DarkRed', 'red', 'DarkKhaki', 'Lime', 'Green', 'DeepSkyBlue', 'Blue', 'Violet', 'black']
-    LINE_COLORS = []
-    for color in COLORS:
-        for style in LINE_STYLES:
-            LINE_COLORS.append(color)
-    NUM_STYLES = len(LINE_STYLES)
-    cm = plt.get_cmap('gist_rainbow')
-    cm = plt.get_cmap('nipy_spectral')
-    
-    x = datetimes
-    fig, ax = plt.subplots(figsize=(14,10))
-    ax.set_xlim(datetimes[0], datetimes[-1])
-    ax.axhspan(ymin, 0.05*days, facecolor='blue', alpha=alpha)
-    ax.axhspan(0.05*days, 0.1*days, facecolor='green', alpha=alpha)
-    ax.axhspan(0.1*days, 0.25*days, facecolor='yellow', alpha=alpha)
-    ax.axhspan(0.25*days, 0.5*days, facecolor='darkorange', alpha=alpha)
-    ax.axhspan(0.5*days, ymax, facecolor='red', alpha=alpha)
-    
-    for i, phu in enumerate(phus[0:]):
-        y = data[phu]
-        #color = cm(i//NUM_STYLES*float(NUM_STYLES)/NUM_COLORS)
-        #color = LINE_COLORS[i]
-        #linestyle = LINE_STYLES[i%NUM_STYLES]
-        #ax.plot(x, y, label=phu, color=color, linestyle=linestyle)
-        ax.plot(x, y, label=phu)
-
-    ax.legend(loc='best', bbox_to_anchor=(0.01, 1),
-              ncol=2, borderaxespad=0, title=' ',#title='Regional public health unit',
-              frameon=False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.set_ylim(ymin, ymax)
-    ax.set_ylabel('# infected cases per 10,000')
-    ax.set_xlabel('Date')
-
-    fig.subplots_adjust(top=0.4)
-    fig.suptitle('Left-click on a PHU name to show/hide it   |   Right-click to hide all   |   Middle-click to show all',
-                 va='top', size='large', y=1)
-
-    leg = interactive_legend()
-    return fig, ax, leg
-
-def interactive_legend(ax=None):
-    if ax is None:
-        ax = plt.gca()
-    if ax.legend_ is None:
-        ax.legend()
-
-    return InteractiveLegend(ax.get_legend())
-
-class InteractiveLegend(object):
-    def __init__(self, legend):
-        self.legend = legend
-        self.fig = legend.axes.figure
-
-        self.lookup_artist, self.lookup_handle = self._build_lookups(legend)
-        self._setup_connections()
-
-        self.update()
-
-    def _setup_connections(self):
-        for artist in self.legend.texts + self.legend.legendHandles:
-            artist.set_picker(10) # 10 points tolerance
-
-        self.fig.canvas.mpl_connect('pick_event', self.on_pick)
-        self.fig.canvas.mpl_connect('button_press_event', self.on_click)
-
-    def _build_lookups(self, legend):
-        labels = [t.get_text() for t in legend.texts]
-        handles = legend.legendHandles
-        label2handle = dict(zip(labels, handles))
-        handle2text = dict(zip(handles, legend.texts))
-
-        lookup_artist = {}
-        lookup_handle = {}
-        for artist in legend.axes.get_children():
-            if artist.get_label() in labels:
-                handle = label2handle[artist.get_label()]
-                lookup_handle[artist] = handle
-                lookup_artist[handle] = artist
-                lookup_artist[handle2text[handle]] = artist
-
-        lookup_handle.update(zip(handles, handles))
-        lookup_handle.update(zip(legend.texts, handles))
-
-        return lookup_artist, lookup_handle
-
-    def on_pick(self, event):
-        handle = event.artist
-        if handle in self.lookup_artist:
-
-            artist = self.lookup_artist[handle]
-            artist.set_visible(not artist.get_visible())
-            self.update()
-
-    def on_click(self, event):
-        if event.button == 3:
-            visible = False
-        elif event.button == 2:
-            visible = True
-        else:
-            return
-
-        for artist in self.lookup_artist.values():
-            artist.set_visible(visible)
-        self.update()
-
-    def update(self):
-        for artist in self.lookup_artist.values():
-            handle = self.lookup_handle[artist]
-            if artist.get_visible():
-                handle.set_visible(True)
-            else:
-                handle.set_visible(False)
-        self.fig.canvas.draw()
-
-    def show(self):
-        plt.show()
-
-if __name__ == '__main__':
-    fig, ax, leg = main()
-    plt.tight_layout()
-    plt.show()
-# -
-
-# ## Visualize the timelines for all PHUs on the same interactive plot
-# *Color/line scheme B*
 
 # +
 # This code was based on https://stackoverflow.com/questions/31410043/hiding-lines-after-showing-a-pyplot-figure/31417070
@@ -358,6 +179,7 @@ def main():
         color = LINE_COLORS[i]
         linestyle = LINE_STYLES[i%NUM_STYLES]
         ax.plot(x, y, label=phu, color=color, linestyle=linestyle)
+        #ax.plot(x, y, label=phu) # default color scheme
 
     ax.legend(loc='best', bbox_to_anchor=(0.01, 1),
               ncol=2, borderaxespad=0, title=' ',#title='Regional public health unit',
@@ -365,12 +187,12 @@ def main():
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.set_ylim(ymin, ymax)
-    ax.set_ylabel('# infected cases per 10,000')
+    ax.set_ylabel('infection rate in the last 14 days\nper 10,000 people\n(case_count / population * 10,000)')
     ax.set_xlabel('Date')
 
     fig.subplots_adjust(top=0.4)
     fig.suptitle('Left-click on a PHU name to show/hide it   |   Right-click to hide all   |   Middle-click to show all',
-                 va='top', size='large', y=1)
+                 va='top', size='large', y=.99)
 
     leg = interactive_legend()
     def line_hover(event):
@@ -463,6 +285,39 @@ if __name__ == '__main__':
     plt.tight_layout()
     
     plt.show()
+    
+plt.savefig('test.')
 # -
+# ## Visualize the timeline for each PHU in a separate plot
 
 
+# +
+# # visualize
+# matplotlib.rc('font', size = 22)
+# matplotlib.rc('figure', max_open_warning = 0)
+
+# ymax = np.max(data.loc[:, data.columns != 'Date'].values) + 2
+# ymin = - .03
+# linewidth = 4
+
+# alpha = .1
+# days = 14
+# for phu in phus:
+    
+#     p = data[phu]
+#     t = datetimes
+    
+#     fig, ax = plt.subplots(figsize=(20, 10))
+    
+#     ax.plot(datetimes, p, color='black', linewidth=linewidth)
+    
+#     ax.axhspan(ymin, 0.05*days, facecolor='blue', alpha=alpha)
+#     ax.axhspan(0.05*days, 0.1*days, facecolor='green', alpha=alpha)
+#     ax.axhspan(0.1*days, 0.25*days, facecolor='yellow', alpha=alpha)
+#     ax.axhspan(0.25*days, 0.5*days, facecolor='darkorange', alpha=alpha)
+#     ax.axhspan(0.5*days, ymax, facecolor='red', alpha=alpha)
+    
+#     plt.ylim([ymin, ymax])
+#     plt.xlabel('Date')
+#     plt.ylabel('# infected per 10,000\nin past 14 days')
+#     plt.title('%s' %phu)
